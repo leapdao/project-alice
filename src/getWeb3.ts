9847713212cbb80a0cf23eb87dbc5d642a1f3f7c
Alex Lunyov <isuntc@gmail.com>
2018-06-27 19:49:55 +0800

    Extend web3 instance with getUnspent method

--- src/getWeb3.ts
+++ src/getWeb3.ts
@@ -1,20 +1,37 @@
 import * as Web3 from "web3";
 import { WEB3_PROVIDER_BASE, WEB3_PROVIDER_BASE_WS } from "./config";
 
-let web3;
+let web3http;
 let web3ws;
+
+const addCustomWeb3Methods = (web3) => {
+  web3.extend({
+    methods: [
+      new web3.extend.Method({
+        name: "getUnspents",
+        call: "parsec_unspents",
+        params: 1,
+        inputFormatters: [
+          web3.extend.formatters.inputAddressFormatter,
+        ],
+        outputFormatter: a => a,
+      })
+    ]
+  });
+  return web3;
+};
 
 export default (ws = true) => {
   if (ws && !web3ws) {
     if (WEB3_PROVIDER_BASE_WS === WEB3_PROVIDER_BASE) {
-      web3ws = web3 || new (Web3 as any)(WEB3_PROVIDER_BASE);
+      web3ws = web3http || addCustomWeb3Methods(new (Web3 as any)(WEB3_PROVIDER_BASE));
     } else {
-      web3ws = new (Web3 as any)(WEB3_PROVIDER_BASE_WS);
+      web3ws = addCustomWeb3Methods(new (Web3 as any)(WEB3_PROVIDER_BASE_WS));
     }
   }
-  if (!ws && !web3) {
-    web3 = new (Web3 as any)(WEB3_PROVIDER_BASE);
+  if (!ws && !web3http) {
+    web3http = addCustomWeb3Methods(new (Web3 as any)(WEB3_PROVIDER_BASE));
   }
 
-  return ws ? web3ws : web3;
+  return ws ? web3ws : web3http;
 };
