20ab3567b45652af1c3079e39a60d88f86df67d8
Alex Lunyov <isuntc@gmail.com>
2018-10-02 23:36:17 +0800

    Fix transfers

--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,21 @@
 
 import * as BN from "bn.js";
 
+const TEN = new BN(10, 10);
+
+function toInt(n: number | string) {
+  const decimals = Math.min(5, Number(String(n).split(".")[1]) || 0);
+  return [Number(n) * (10 ** decimals), decimals];
+}
+
 export function toCents(n: number | string, decimals: number) {
-  return new BN(n, 10).mul(new BN(10 ** decimals, 10)).toNumber();
+  const [int, nDecimals] = toInt(n); // BN.js doesn't support decimals
+  const decimalsBN = new BN(decimals - nDecimals, 10);
+  return (
+    new BN(int, 10)
+      .mul(TEN.pow(decimalsBN))
+      .toNumber()
+  );
 }
 
 export function fromCents(n: number | string, decimals: number) {
